0e736d8a3591c0fd59a1183dd78524b384dd1e76
shalina maniyar <shalinamaniyar@gmail.com>
2025-10-04 10:50:19 +0530

    version

--- even_odd.py
+++ even_odd.py
@@ -8,5 +8,13 @@ for i in range(n):
         even_count += 1
     else:
         odd_count += 1
+prime_count = 0
+if num > 1:
+        for j in range(2, int(num**0.5) + 1):
+            if num % j == 0:
+                break
+        else:
+            prime_count += 1
 print("\nTotal Even Numbers:", even_count)
-print("Total Odd Numbers:", odd_count)
+print("\nTotal Odd Numbers:", odd_count)
+print("\nTotal Prime Numbers:", prime_count)
